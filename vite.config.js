f336b034698500fb8f08e1acedda91af0b33bec3
Nima <nimarahimloo2000@gmail.com>
2026-08-23 20:44:29 +0330

    build(vite): register brand stylesheet entry

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/admin.css',
+                'resources/css/brand.css',
                 'resources/js/app.js',
                 'resources/js/admin.js',
             ],
