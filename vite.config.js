90b20a50276445f29cbe5380f6c9b84dfa2a55c5
Nima <nimarahimloo2000@gmail.com>
2026-08-24 19:48:34 +0330

    feat(menu): register QRMenu-inspired stylesheet

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/admin.css',
+                'resources/css/menu-redesign.css',
                 'resources/js/app.js',
                 'resources/js/admin.js',
             ],
